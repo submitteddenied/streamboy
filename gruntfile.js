7230a17eb6c21de3f5688f8f15bc8bf8f334aad9
Michael Jensen <mjensen@riotgames.com>
2016-02-11 01:43:39 -0800

    Add livereload

--- gruntfile.js
+++ gruntfile.js
@@ -34,17 +34,43 @@ module.exports = function(grunt) {
           }
         ]
       }
+    },
+    express: {
+      dev: {
+        options: {
+          script: 'main.js'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: 8083
+      },
+      js: {
+        files: ['src/**/*.js'],
+        tasks: ['browserify:dist']
+      },
+      css: {
+        files: ['src/**/*.scss'],
+        tasks: ['sass']
+      },
+      public: {
+        files: ['public/**/*'],
+        tasks: ['copy']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('build', ['browserify:dist']);
 
   grunt.registerTask('compile', ['browserify:dist', 'sass', 'copy']);
 
-  grunt.registerTask('dev', ['compile']);
+  grunt.registerTask('dev', ['compile', 'express:dev', 'watch']);
   grunt.registerTask('default', 'dev');
 };
--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
 var React = require('react');
+var dummyData = require('../db.json');
 var Loading = require('./components/loading');
 var GameUI = require('./components/game-ui');
 
 // Your code here
 window.onload = function() {
   React.render(
-    <Loading />,
+    <GameUI data={dummyData} />,
     document.getElementById('content')
   );
 
-  window.refreshInterval = window.setInterval(function() {
-    $.ajax({
-      url: 'data.json',
-      success: updateFrame
-    });
-  }, 1000);
+  //window.refreshInterval = window.setInterval(function() {
+    //$.ajax({
+      //url: 'data.json',
+      //success: updateFrame
+    //});
+  //}, 1000);
 }
 
 function updateFrame(data) {
